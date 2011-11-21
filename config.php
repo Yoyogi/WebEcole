eb628cd910c672cea36f57a1f6db1db447313e79
Yoyogi <yojinshi@hotmail.fr>
2011-11-21 10:46:55 +0100

    commit 21-11-2011

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/* nom des fichiers principaux */
+$controler_file = "controler.php";
+$header_file = "header.php";
+$footer_file = "footer.php";
+
+/* nom des fichiers de style */
+$styleGeneral_file = "style.css";
+
+/* nom des répertoires */
+$auth_repo = "auth/";
+$adm_repo = "adm/";
+$pupil_repo = "pupil/";
+$teacher_repo = "teacher/";
+
+/* nom des fichiers de page */
+$index_file = $auth_repo."auth.php";
+?>
---
+++ controler.php
@@ -0,0 +1,15 @@
+<?php
+switch($page) {
+    case "auth":
+        $body = $index_file;
+        $title = "Connexion Web Ecole";
+        $metadescription = "Web Ecole - Connexion";
+        break;
+    
+    default:
+        $body = $index_file;
+        $title = "Connexion Web Ecole";
+        $metadescription = "Web Ecole - Connexion";
+        break;
+}
+?>
