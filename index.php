783863ac0249965c29b7b99d297e0afc6ea7c173
Yoyogi <yojinshi@hotmail.fr>
2011-11-21 10:45:26 +0100

    commit 21-11-2011

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+@set_time_limit(900); //15min.
+
+header('Content-type: text/html; charset=iso-8859-1');
+
+//remplace GET et POST par les noms de variables
+foreach ($_GET as $key => $value) $$key = $value;
+foreach ($_POST as $key => $value) $$key = $value;
+
+include_once "config.php";
+
+//initialisation des variables de navigation pour la premiere utilisation
+if(!isset($page))
+    $page = "";
+if(!isset($spage))
+    $spage = "";
+
+include $controler_file;
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title><?php echo $title; ?></title>
+        <meta name="description" content="<?php echo $metadescription; ?>" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Language" content="fr" />
+        <link href="<?php echo $styleGeneral_file; ?>" rel="stylesheet" type="text/css" media="screen,print" />
     </head>
+    
     <body>
         <?php
-        // put your code here
+            include $body;
         ?>
     </body>
 </html>
