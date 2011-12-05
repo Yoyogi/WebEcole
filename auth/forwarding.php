d604ba23af3cf0d744560ebe6851e165d1be0131
Sebastien Ferrand <sebastien.ferrand.dev@gmail.com>
2011-12-05 11:38:30 +0100

    poupou

--- auth/forwarding.php
+++ auth/forwarding.php
@@ -4,19 +4,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+header('loggedAccueil.htm');
+
+header('accueil.htm');
+
 ?>
 
-<form method="POST" action="addEleve.htm">
-    <p><label> Identififiant </label> <input type=text name=login> </p>
-    <p><label> Mot de passe </label> <input type=text name=password> </p>
-    <p><label> Nom </label> <input type=text name=login> </p>
-    <p><label> Prénom </label> <input type=text name=password> </p>
-    <p><label> Email </label> <input type=text name=login> </p>
-    <p><label> Status </label> <input type=text name=password> </p>
-    
-    
-    <p><label> Photo </label> <input type=text name=login> </p>
-    <p><label> Date de naissance </label> <input type=text name=password> </p>
-    
-    <p> <input type="submit" name="envoyer" value="Se connecter"> </p>
-</form>
+<!-- ici redirection en fonction de ce qui est trouvé en base données -->
+
+
