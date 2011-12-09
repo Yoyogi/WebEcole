3a9d248e8c2578bc4c256b8b726630d5559e1545
Yoyogi <yojinshi@hotmail.fr>
2011-12-09 09:35:42 +0100

    update crud

--- crud/Crud.php
+++ crud/Crud.php
@@ -8,6 +8,9 @@ class Crud implements ICrud {
         $connection = Doctrine_Manager::connection(CFG_DB_DSN);
     }
     
+    //---------------------------------------------------
+    /* CRUD ETUDIANT */
+    //---------------------------------------------------
     function createStudent($nom, $prenom, $date_naissance, $rue, $cp, $ville, $email, $ulogin, $passwd, $photo) {
         $student = new Student();
         $student->nom = $nom;
@@ -24,9 +27,80 @@ class Crud implements ICrud {
         $student->save();
     }
     
+    function updateStudent($idStudent, $nom, $prenom, $date_naissance, $rue, $cp, $ville, $email, $ulogin, $passwd, $photo) {
+        if (Doctrine_Core::getTable("Student")->find($idStudent)) {
+            $student = $this->getStudentById($idStudent);
+            $student->nom = $nom;
+            $student->prenom = $prenom;
+            $student->date_naissance = $date_naisse;
+            $student->rue = $rue;
+            $student->cp = $cp;
+            $student->ville = $ville;
+            $student->email = $email;
+            $student->ulogin = $ulogin;
+            $student->passwd = $passwd;
+            $student->photo = $photo;
+
+            $student->save();
+        }
+    }
+    
+    function deleteStudent($idStudent) {
+        if (Doctrine_Core::getTable("Student")->find($idStudent)) {
+            $student = Doctrine_Core::getTable("Student")->find($idStudent);
+            $student->delete();
+        }
+    }
+    
     function getStudents() {
         return Doctrine_Core::getTable("Student")->findAll();
     }
+    
+    function getStudentById($id) {
+        return Doctrine_Core::getTable("Student")->find($id);
+    }
+    
+    //---------------------------------------------------
+    /* CRUD ENSEIGNANT */
+    //---------------------------------------------------
+    function createTeacher($nom, $prenom, $date_naissance, $rue, $cp, $ville, $email, $ulogin, $passwd, $photo);
+    function updateTeacher($nom, $prenom, $date_naissance, $rue, $cp, $ville, $email, $ulogin, $passwd, $photo);
+    function deleteTeacher($idTeacher);
+    function getTeachers() {
+        return Doctrine_Core::getTable("Teacher")->findAll();
+    }
+    function getTeacherById($id);
+    
+    
+    //---------------------------------------------------
+    /* CRUD ENSEIGNANT */
+    //---------------------------------------------------
+    function getAdmins();
+    function getAdminById($id);
+    
+    
+    //---------------------------------------------------
+    /* CRUD ABSENCES */
+    //---------------------------------------------------
+    function getAbsencesBylesson($idLesson);
+    function getAbsencesByStudent($idStudent);
+    
+    
+    //---------------------------------------------------
+    /* CRUD COURS */
+    //---------------------------------------------------
+    function getLessons();
+    function getLessonsByTeacher($idTeacher);
+    function getLessonsBySubject($idSubject);
+    function getLessonsByPromotion($idPromotion);
+    
+    
+    //---------------------------------------------------
+    /* CRUD MATIERES */
+    //---------------------------------------------------
+    function getSubjects();
+    function getSubjectsByTeacher($idTeacher);
+    function getSubjectsByLesson($idLesson);
 }
 
 ?>
