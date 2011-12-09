1a2de83bf13c3a11c3b29a3288c40a762ccd193c
veilly <florent.veilly@orange.fr>
2011-12-09 08:56:30 +0100

    je sais pas

--- adm/gest_personne.php
+++ adm/gest_personne.php
@@ -4,15 +4,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-    $header = array("identifiant", "nom", "prenom", "status");
+    $header = array('Identifiant' => 'identifiant', 'Nom' => 'nom', 'Prenom' => 'prenom', 'Status' => 'status');
 ?>
 
 <table border="1">
     <caption align="center">Gestion des personnes</caption>
     <tr bgcolor="#ff0000">
     <?php
-        foreach ($header as $i => $value) {
-            ?><th rowspan="2"><?php echo $array[$i]; ?></th><?php
+        foreach ($header as $id => $value) {
+            ?><th rowspan="2" width="50"><?php echo $id ?></th><?php
         }
         ?><th rowspan="2">Actions</th><?php
     ?>
