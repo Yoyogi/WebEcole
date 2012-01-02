80bd818ca1d063359a85cae91375aa15f212bc91
Yoyogi <yojinshi@hotmail.fr>
2012-01-02 18:27:03 +0100

    crud updated and completed

--- crud/Crud.php
+++ crud/Crud.php
@@ -60,6 +60,78 @@ class Crud implements ICrud {
         }
     }
     
+    function addPromotionToStudent(Etudiant $student, Promotion $promotion) {
+        if (!$student->Promotions->contains($promotion)) {
+            $student->Promotions->add($promotion);
+            $student->Promotions->save();
+        }
+        
+        if (!$promotion->Etudiants->contains($student)) {
+            $promotion->Etudiants->add($student);
+            $promotion->Etudiants->save();
+        }
+        
+        $sp = new EtudiantPromotion();
+        $sp->id_etudiant = $student->id_etudiant;
+        $sp->id_promo = $promotion->id_promo;
+        $sp->Etudiant = $student;
+        $sp->Promotion = $promotion;
+        
+        if (!$student->EtudiantPromotion->contains($sp)) {
+            $student->EtudiantPromotion->add($sp);
+            $student->EtudiantPromotion->save();
+        }
+        
+        if (!$promotion->EtudiantPromotion->contains($sp)) {
+            $promotion->EtudiantPromotion->add($sp);
+            $promotion->EtudiantPromotion->save();
+        }
+        
+        $r_sp = Doctrine_Core::getTable("EtudiantPromotion")->findOneById_etudiantAndId_promotion($student->id_etudiant, $promotion->id_promo);
+        if ($r_sp == null) {
+            $sp->save();
+        }
+        
+        $student->save();
+        $promotion->save();
+    }
+    
+    function removePromotionToStudent(Etudiant $student, Promotion $promotion) {
+        $sp = Doctrine_Core::getTable("EtudiantPromotion")->findOneById_etudiantAndId_promotion($student->id_etudiant, $promotion->id_promo);
+        if ($sp != null) {
+            $student->EtudiantPromotion->remove($sp);
+            $student->EtudiantPromotion->save();
+            $promotion->EtudiantPromotion->remove($sp);
+            $promotion->EtudiantPromotion->save();
+            $sp->delete();
+            
+            $student->Promotions->remove($promotion);
+            $student->Promotions->save();
+            
+            $promotion->Etudiants->remove($student);
+            $promotion->Etudiants->save();
+            
+            $student->save();
+            $promotion->save();
+        }
+    }
+    
+    function addAbsenceToStudent(Absence $absence, Etudiant $student) {
+        if (!$student->Absences->contains($absence)) {
+            $student->Absences->add($absence);
+            $student->Absences->save();
+            $student->save();
+        }
+    }
+    
+    function removeAbsenceToStudent(Absence $absence, Etudiant $student) {
+        if (!$student->Absences->contains($absence)) {
+            $student->Absences->remove($absence);
+            $student->Absences->save();
+            $student->save();
+        }
+    }
+    
     function getStudents() {
         $students = Doctrine_Core::getTable("Etudiant")->findAll();
         if ($students != null) {
@@ -124,6 +196,78 @@ class Crud implements ICrud {
         }
     }
     
+    function addSubjectToTeacher(Enseignant $enseignant, Matiere $matiere) {
+        if (!$enseignant->Matieres->contains($matiere)) {
+            $enseignant->Matieres->add($matiere);
+            $enseignant->Matieres->save();
+        }
+            
+        if (!$matiere->Enseignants->contains($enseignant)) {
+            $matiere->Enseignants->add($enseignant);
+            $matiere->Enseignants->save();
+        }
+            
+        $em = new EnseignantMatiere();
+        $em->id_enseignant = $enseignant->id_enseignant;
+        $em->id_matiere = $matiere->id_matiere;
+        $em->Enseignant = $enseignant;
+        $em->Matiere = $matiere;
+        
+        if (!$enseignant->EnseignantMatiere->contains($em)) {
+            $enseignant->EnseignantMatiere->add($em);
+            $enseignant->EnseignantMatiere->save();
+        }
+
+        if (!$matiere->EnseignantMatiere->contains($em)) {
+            $matiere->EnseignantMatiere->add($em);
+            $matiere->EnseignantMatiere->save();
+        }
+        
+        $r_em = Doctrine_Core::getTable("EnseignantMatiere")->findOneById_enseignantAndId_matiere($enseignant->id_enseignant, $matiere->id_matiere);
+        if ($r_em == null) {
+            $em->save();
+        }
+        
+        $enseignant->save();
+        $matiere->save();
+    }
+    
+    function removeSubjectToTeacher(Enseignant $enseignant, Matiere $matiere) {
+        $em = Doctrine_Core::getTable("EnseignantMatiere")->findOneById_enseignantAndId_matiere($enseignant->id_enseignant, $matiere->id_matiere);
+        if ($em != null) {
+            $enseignant->EnseignantMatiere->remove($em);
+            $enseignant->EnseignantMatiere->save();
+            $matiere->EnseignantMatiere->remove($em);
+            $matiere->EnseignantMatiere->save();
+            $em->delete();
+            
+            $enseignant->Matieres->remove($matiere);
+            $enseignant->Matieres->save();
+            
+            $matiere->Enseignants->remove($enseignant);
+            $matiere->Enseignants->save();
+            
+            $enseignant->save();
+            $matiere->save();
+        }
+    }
+    
+    function addLessonToTeacher(Enseignant $enseignant, Cours $cours) {
+        if (!$enseignant->Cours->contains($cours)) {
+            $enseignant->Cours->add($cours);
+            $enseignant->Cours->save();
+            $enseignant->save();
+        }
+    }
+    
+    function removeLessonToTeacher(Enseignant $enseignant, Cours $cours) {
+        if (!$enseignant->Cours->contains($cours)) {
+            $enseignant->Cours->remove($cours);
+            $enseignant->Cours->save();
+            $enseignant->save();
+        }
+    }
+    
     function getTeachers() {
         $teachers = Doctrine_Core::getTable("Enseignant")->findAll();
         if ($teachers != null) {
@@ -180,7 +324,7 @@ class Crud implements ICrud {
     //---------------------------------------------------
     /* CRUD ABSENCES */
     //---------------------------------------------------
-    function createAbsence($motif, $etudiant, $cours) {
+    function createAbsence($motif, Etudiant $etudiant, Cours $cours) {
         $absence = new Absence();
         $absence->motif = $motif;
         $absence->id_etudiant = $etudiant->id_etudiant;
@@ -190,7 +334,7 @@ class Crud implements ICrud {
         $absence->save();
     }
     
-    function updateAbsence($id, $motif, $etudiant, $cours) {
+    function updateAbsence($id, $motif, Etudiant $etudiant, Cours $cours) {
         if (Doctrine_Core::getTable("Absence")->findOneBy("id_absence", $id)) {
             $absence = Doctrine_Core::getTable("Absence")->findOneBy("id_absence", $id);
             $absence->motif = $motif;
@@ -238,7 +382,7 @@ class Crud implements ICrud {
     //---------------------------------------------------
     /* CRUD COURS */
     //---------------------------------------------------
-    function createLesson($date, $duree, $descript, $enseignant, $promotion, $matiere) {
+    function createLesson($date, $duree, $descript, Enseignant $enseignant, Promotion $promotion, Matiere $matiere) {
         $lesson = new Cours();
         $lesson->date_cours = $date;
         $lesson->duree = $duree;
@@ -253,7 +397,7 @@ class Crud implements ICrud {
         $lesson->save();
     }
     
-    function updateLesson($id, $date, $duree, $descript, $enseignant, $promotion, $matiere) {
+    function updateLesson($id, $date, $duree, $descript, Enseignant $enseignant, Promotion $promotion, Matiere $matiere) {
         if (Doctrine_Core::getTable("Cours")->findOneBy("id_cours", $id)) {
             $lesson = Doctrine_Core::getTable("Cours")->findOneBy("id_cours", $id);
             $lesson->date_cours = $date;
@@ -277,7 +421,7 @@ class Crud implements ICrud {
         }
     }
     
-    function addAbsence($absence) {
+    function addAbsenceToLesson(Cours $lesson, Absence $absence) {
         if (!$lesson->Absences->contains($absence)) {
             $lesson->Absences->add($absence);
             $lesson->Absences->save();
@@ -285,7 +429,7 @@ class Crud implements ICrud {
         }
     }
     
-    function removeAbsence($absence) {
+    function removeAbsenceToLesson(Cours $lesson, Absence $absence) {
         if (!$lesson->Absences->contains($absence)) {
             $lesson->Absences->remove($absence);
             $lesson->Absences->save();
@@ -293,7 +437,7 @@ class Crud implements ICrud {
         }
     }
     
-    function addExercice($exercice) {
+    function addExerciceToLesson(Cours $lesson, Exercice $exercice) {
         if (!$lesson->Exercices->contains($exercice)) {
             $lesson->Exercices->add($exercice);
             $lesson->Exercices->save();
@@ -301,7 +445,7 @@ class Crud implements ICrud {
         }
     }
     
-    function removeExercice($exercice) {
+    function removeExerciceToLesson(Cours $lesson, Exercice $exercice) {
         if (!$lesson->Exercices->contains($exercice)) {
             $lesson->Absences->remove($exercice);
             $lesson->Exercices->save();
@@ -353,6 +497,53 @@ class Crud implements ICrud {
     //---------------------------------------------------
     /* CRUD MATIERES */
     //---------------------------------------------------
+    function createSubject($libelle) {
+        $subject = new Matiere();
+        $subject->libelle = $libelle;
+        
+        $subject->save();
+    }
+    
+    function updateSubject($id, $libelle) {
+        if (Doctrine_Core::getTable("Matiere")->findOneBy("id_matiere", $id)) {
+            $subject = Doctrine_Core::getTable("Matiere")->findOneBy("id_matiere", $id);
+            $subject->libelle = $libelle;
+            
+            $subject->save();
+        }
+    }
+    
+    function deleteSubject($id) {
+        if (Doctrine_Core::getTable("Matiere")->findOneBy("id_matiere", $id)) {
+            $subject = Doctrine_Core::getTable("Matiere")->findOneBy("id_matiere", $id);
+            $subject->delete();
+        }
+    }
+    
+    function addTeacherToSubject(Matiere $subject, Enseignant $teacher) {
+        $this->addSubjectToTeacher($teacher, $subject);
+    }
+    
+    function removeTeacherToSubject(Matiere $subject, Enseignant $teacher) {
+        $this->removeSubjectToTeacher($teacher, $subject);
+    }
+    
+    function addLessonToSubject(Matiere $subject, Cours $lesson) {
+        if (!$subject->Cours->contains($lesson)) {
+            $subject->Cours->add($lesson);
+            $subject->Cours->save();
+            $subject->save();
+        }
+    }
+    
+    function removeLessonToSubject(Matiere $subject, Cours $lesson) {
+        if (!$subject->Cours->contains($lesson)) {
+            $subject->Cours->remove($lesson);
+            $subject->Cours->save();
+            $subject->save();
+        }
+    }
+    
     function getSubjects() {
         $subjects = Doctrine_Core::getTable("Matiere")->findAll();
         if ($subjects != null) {
@@ -377,6 +568,207 @@ class Crud implements ICrud {
         $subject = Doctrine_Core::getTable("Matiere")->findOneBy("id_matiere", $idSubject);
         if ($subject != null) {
             return $subject;
+        }
+        return null;
+    }
+    
+    
+    //---------------------------------------------------
+    /* CRUD AIDE */
+    //---------------------------------------------------
+    function createHelp($page, $libelle) {
+        $help = new Aide();
+        $help->libelle = $libelle;
+        $help->page = $page;
+        
+        $help->save();
+    }
+    
+    function updateHelp($id, $page, $libelle) {
+        if (Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id)) {
+            $help = Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id);
+            $help->libelle = $libelle;
+            $help->page = $page;
+            
+            $help->save();
+        }
+    }
+    
+    function deleteHelp($id) {
+        if (Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id)) {
+            $help = Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id);
+            
+            $help->delete();
+        }
+    }
+    
+    function getHelps() {
+        $helps = Doctrine_Core::getTable("Aide")->findAll();
+        if ($helps != null) {
+            return $helps;
+        }
+        return null;
+    }
+    
+    function getHelpById($id) {
+        if (Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id)) {
+            return Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id);
+        }
+        return null;
+    }
+    
+    function getHelpsByPage($page) {
+        if (Doctrine_Core::getTable("Aide")->findBy("page", $page)) {
+            return Doctrine_Core::getTable("Aide")->findOneBy("id_aide", $id);
+        }
+        return null;
+    }
+    
+    
+    //---------------------------------------------------
+    /* CRUD PROMOTION */
+    //---------------------------------------------------
+    function createPromotion($libelle) {
+        $promotion = new Promotion();
+        $promotion->libelle = $libelle;
+        
+        $promotion->save();
+    }
+    
+    function updatePromotion($id, $libelle) {
+        $promotion = Doctrine_Core::getTable("Promotion")->findOneBy("id_promotion", $id);
+        if ($promotion != null) {
+            $promotion->libelle = $libelle;
+            $promotion->save();
+        }
+    }
+    
+    function deletePromotion($id) {
+        $promotion = Doctrine_Core::getTable("Promotion")->findOneBy("id_promotion", $id);
+        if ($promotion != null) {
+            $promotion->delete();
+        }
+    }
+    
+    function addStudentToPromotion(Promotion $promotion, Etudiant $student) {
+        $this->addPromotionToStudent($student, $promotion);
+    }
+    
+    function removeStudentFromPromotion(Promotion $promotion, Etudiant $student) {
+        $this->removePromotionToStudent($student, $promotion);
+    }
+    
+    function addLessonToPromotion(Promotion $promotion, Cours $lesson) {
+        if (!$promotion->Cours->contains($lesson)) {
+            $promotion->Cours->add($lesson);
+            $promotion->Cours->save();
+            $promotion->save();
+        }
+    }
+    
+    function removeLessonFromPromotion(Promotion $promotion, Cours $lesson) {
+        if (!$promotion->Cours->contains($lesson)) {
+            $promotion->Cours->remove($lesson);
+            $promotion->Cours->save();
+            $promotion->save();
+        }
+    }
+    
+    function getPromotions() {
+        $promotions = Doctrine_Core::getTable("Promotion")->findAll();
+        if ($promotions != null) {
+            return $promotions;
+        }
+        return null;
+    }
+    
+    function getPromotionById($id) {
+        $promotion = Doctrine_Core::getTable("Promotion")->findOneBy("id_promotion", $id);
+        if ($promotion != null) {
+            return $promotion;
+        }
+        return null;
+    }
+    
+    function getPromotionsByStudent($student) {
+        $results = array ();
+        $promotions = $this->getPromotions();
+        $i = 0;
+        foreach ($promotions as $promotion) {
+            if ($promotion->Etudiants->contains($student)) {
+                $results[$i] = $promotion;
+                $i++;
+            }
+        }
+        
+        return $results;
+    }
+    
+    function getPromotionsByLesson($lesson) {
+        $results = array ();
+        $promotions = $this->getPromotions();
+        $i = 0;
+        foreach($promotions as $promotion) {
+            if ($promotion->Cours->contains($lesson)) {
+                $results[$i] = $promotion;
+                $i++;
+            }
+        }
+        
+        return $results;
+    }
+    
+    
+    //---------------------------------------------------
+    /* CRUD EXERCICE */
+    //---------------------------------------------------
+    function createExercice($libelle, Cours $lesson) {
+        $exercice = new Exercice();
+        $exercice->libelle = $libelle;
+        $exercice->id_cours = $lesson->id_cours;
+        $exercice->Cours = $lesson;
+        
+        $exercice->save();
+    }
+    
+    function updateExercice($id, $libelle, Cours $lesson) {
+        $exercice = Doctrine_Core::getTable("Exercice")->findOneBy("id_exercice", $id);
+        if ($exercice != null) {
+            $exercice->libelle = $libelle;
+            $exercice->id_cours = $lesson->id_cours;
+            $exercice->Cours = $lesson;
+            
+            $exercice->save();
+        }
+    }
+    
+    function deleteExercice($id) {
+        $exercice = Doctrine_Core::getTable("Exercice")->findOneBy("id_exercice", $id);
+        if ($exercice != null) {
+            $exercice->delete();
+        }
+    }
+    
+    function getExercices() {
+        $exercices = Doctrine_Core::getTable("Exercice")->findAll();
+        if ($exercices != null) {
+            return $exercices;
+        }
+        return null;
+    }
+    
+    function getExerciceById($id) {
+        $exercice = Doctrine_Core::getTable("Exercice")->findOneBy("id_exercice", $id);
+        if ($exercice != null) {
+            return $exercice;
+        }
+        return null;
+    }
+    
+    function getExercicesByLesson($id_lesson) {
+        $exercices = Doctrine_Core::getTable("Exercice")->findBy("id_cours", $id_lesson);
+        if ($exercices != null) {
+            return $exercices;
         }
         return null;
     }
--- crud/ICrud.php
+++ crud/ICrud.php
@@ -17,6 +17,10 @@ interface ICrud {
     function createStudent($nom, $prenom, $date_naissance, $rue, $cp, $ville, $email, $ulogin, $passwd, $photo);
     function updateStudent($idStudent, $nom, $prenom, $date_naissance, $rue, $cp, $ville, $email, $ulogin, $passwd, $photo);
     function deleteStudent($idStudent);
+    function addPromotionToStudent(Etudiant $student, Promotion $promotion);
+    function removePromotionToStudent(Etudiant $student, Promotion $promotion);
+    function addAbsenceToStudent(Absence $absence, Etudiant $student);
+    function removeAbsenceToStudent(Absence $absence, Etudiant $student);
     function getStudents();
     function getStudentById($id);
     function getStudentByLogin($login);
@@ -25,6 +29,10 @@ interface ICrud {
     function createTeacher($nom, $prenom, $rue, $cp, $ville, $email, $ulogin, $passwd);
     function updateTeacher($id, $nom, $prenom, $rue, $cp, $ville, $email, $ulogin, $passwd);
     function deleteTeacher($idTeacher);
+    function addSubjectToTeacher(Enseignant $enseignant, Matiere $matiere);
+    function removeSubjectToTeacher(Enseignant $enseignant, Matiere $matiere);
+    function addLessonToTeacher(Enseignant $enseignant, Cours $cours);
+    function removeLessonToTeacher(Enseignant $enseignant, Cours $cours);
     function getTeachers();
     function getTeacherById($id);
     function getTeacherByLogin($login);
@@ -35,21 +43,21 @@ interface ICrud {
     function getAdminByLogin($login);
     
     //crud absence
-    function createAbsence($motif, $etudiant, $cours);
-    function updateAbsence($id, $motif, $etudiant, $cours);
+    function createAbsence($motif, Etudiant $etudiant, Cours $cours);
+    function updateAbsence($id, $motif, Etudiant $etudiant, Cours $cours);
     function deleteAbsence($id);
     function getAbsences();
     function getAbsencesBylesson($idLesson);
     function getAbsencesByStudent($idStudent);
     
     //crud cours
-    function createLesson($date, $duree, $descript, $enseignant, $promotion, $matiere);
-    function updateLesson($id, $date, $duree, $descript, $enseignant, $promotion, $matiere);
+    function createLesson($date, $duree, $descript, Enseignant $enseignant, Promotion $promotion, Matiere $matiere);
+    function updateLesson($id, $date, $duree, $descript, Enseignant $enseignant, Promotion $promotion, Matiere $matiere);
     function deleteLesson($id);
-    function addAbsence($absence);
-    function removeAbsence($absence);
-    function addExercice($exercice);
-    function removeExercice($exercice);
+    function addAbsenceToLesson(Cours $lesson, Absence $absence);
+    function removeAbsenceToLesson(Cours $lesson, Absence $absence);
+    function addExerciceToLesson(Cours $lesson, Exercice $exercice);
+    function removeExerciceToLesson(Cours $lesson, Exercice $exercice);
     function getLessons();
     function getLessonById($id);
     function getLessonsByTeacher($idTeacher);
@@ -57,9 +65,45 @@ interface ICrud {
     function getLessonsByPromotion($idPromotion);
     
     //crud matière
+    function createSubject($libelle);
+    function updateSubject($id, $libelle);
+    function deleteSubject($id);
+    function addTeacherToSubject(Matiere $subject, Enseignant $teacher);
+    function removeTeacherToSubject(Matiere $subject, Enseignant $teacher);
+    function addLessonToSubject(Matiere $subject, Cours $lesson);
+    function removeLessonToSubject(Matiere $subject, Cours $lesson);
     function getSubjects();
     function getSubjectsByTeacher($idTeacher);
     function getSubjectByLesson($idLesson);
+    
+    //crud aide
+    function createHelp($page, $libelle);
+    function updateHelp($id, $page, $libelle);
+    function deleteHelp($id);
+    function getHelps();
+    function getHelpById($id);
+    function getHelpsByPage($page);
+    
+    //crud promotion
+    function createPromotion($libelle);
+    function updatePromotion($id, $libelle);
+    function deletePromotion($id);
+    function addStudentToPromotion(Promotion $promotion, Etudiant $student);
+    function removeStudentFromPromotion(Promotion $promotion, Etudiant $student);
+    function addLessonToPromotion(Promotion $promotion, Cours $lesson);
+    function removeLessonFromPromotion(Promotion $promotion, Cours $lesson);
+    function getPromotions();
+    function getPromotionById($id);
+    function getPromotionsByStudent($student);
+    function getPromotionsByLesson($lesson);
+    
+    //crud exercice
+    function createExercice($libelle, Cours $lesson);
+    function updateExercice($id, $libelle, Cours $lesson);
+    function deleteExercice($id);
+    function getExercices();
+    function getExerciceById($id);
+    function getExercicesByLesson($id_lesson);
 }
 
 ?>
