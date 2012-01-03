ced0eaf7510bd192d60ede2cae727bba24d9d1d1
veilly <florent.veilly@orange.fr>
2012-01-03 13:22:35 +0100

    crud correct

---
+++ Manager.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Manage
+ *
+ * @author Midgard
+ */
+class Manager {
+    public static $ADMIN = 0;
+    public static $TEACHER = 1;
+    public static $STUDENT = 2;
+    
+    public function getStatus($str) {
+        if ($str == "Admin") {
+            return ManagePeople::$ADMIN;
+        }
+        else if ($str == "Teacher") {
+            return ManagePeople::$TEACHER;
+        }
+        else {
+            return ManagePeople::$STUDENT;
+        }
+    } 
+}
+
+?>
