e61826b03895b32154844577cc0ca0171c4b105d
veilly <florent.veilly@orange.fr>
2012-01-10 20:26:48 +0100

    modifyPeople

--- adm/view/modifyPeople.php
+++ adm/view/modifyPeople.php
@@ -16,134 +16,87 @@ else {
 }
 
 //Vérification de remplissagge des champs en post
-require_once $add_people_class;
-$add_people = AddPeople::getInstance();
+require_once $modify_people_class;
+$modify_people = ModifyPeople::getInstance();
+    
+if ($v_type == Manager::$TEACHER){
+    if (($login != null) && ($password != null) && ($lastName != null)
+        && ($firstName != null) && ($email != null) && ($street != null) 
+            && ($zipcode != null) && ($city != null)){
 
-if (isset($status)){
-    
-    if ($status=="2"){
-        if (($login != null) && ($password != null) && ($lastName != null)
-            && ($firstName != null) && ($email != null) && ($street != null) 
-                && ($zipcode != null) && ($city != null)){
-            
-            try {
-                $add_people->addTeacher($lastName, $firstName, $street, $zipcode, $city, $email, $login, $password);
-                echo "enseignant ajoute";
-            }
-            catch (Exception $e) {
-                echo $e->getMessage();
-            }
-        }else{
-            echo "Login : " . $login;
-            echo "password : " . $password;
-            echo "lastname : " . $lastName;
-            echo "firstname : " . $firstName;
-            echo "email : " . $email;
-            echo "street : " . $street;
-            echo "zipcode : " . $zipcode;
-            echo "city : " . $city;
-            echo "Veuillez remplir tous les champs correctement";
+        try {
+            $modify_people->updateTeacher($lastName, $firstName, $street, $zipcode, $city, $email, $login, $password);
+            echo "enseignant ajoute";
         }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }else{
+        echo "Login : " . $login;
+        echo "password : " . $password;
+        echo "lastname : " . $lastName;
+        echo "firstname : " . $firstName;
+        echo "email : " . $email;
+        echo "street : " . $street;
+        echo "zipcode : " . $zipcode;
+        echo "city : " . $city;
+        echo "Veuillez remplir tous les champs correctement";
     }
-    
-    if ($status=="1"){
-        
-        if (($login != null) && ($password != null) && ($lastName != null)
-            && ($firstName != null) && ($email != null) && ($street != null) 
-                && ($zipcode != null) && ($city != null) && ($photo != null) && ($birthDay != null)){
-            try {
-                $add_people->addPupil($lastName, $firstName, $birthDay, $street, $zipcode, $city, $email, $login, $password, $photo);
-                echo "eleve ajoute";
-            }
-            catch (Exception $e) {
-                echo $e->getMessage();
-            }
-        }
-        else {
-            echo "<br>Login : " . ($login != null);
-            echo "<br>password : " . ($password != 0);
-            echo "<br>lastname : " . ($lastName != 0);
-            echo "<br>firstname : " . ($firstName != 0);
-            echo "<br>email : " . ($email != 0);
-            echo "<br>street : " . ($street != 0);
-            echo "<br>zipcode : " . ($zipcode != null);
-            echo "<br>city : " . ($city != 0);
-            echo "<br>photo : " . ($photo != 0);
-            echo "<br>birthday : " . ($birthDay != 0);
-            echo "<br>Veuillez remplir tous les champs correctement";
-        
-        }
-    }
-    
-    
-}else{
-    echo "Choisissez une categorie";
+
+    $teacher = $modify_people->getPeopleByID($v_id, $v_type);
+    ?>
+    <p><span class="label_form"><label>Login</label></span><input class="input_form" type="text" name="login" value="<?php echo $teacher->ulogin; ?>"></p>
+    <p><span class="label_form"><label>Mot de passe</label></span><input class="input_form" type="text" name="password" value="<?php echo $teacher->passwd; ?>"></p>
+    <p><span class="label_form"><label>Nom</label></span><input class="input_form" type="text" name="lastName" value="<?php echo $teacher->nom; ?>"></p>
+    <p><span class="label_form"><label>Prenom</label></span><input class="input_form" type="text" name="firstName" value="<?php echo $teacher->prenom; ?>"></p>
+    <p><span class="label_form"><label>Rue</label></span><input class="input_form" type="text" name="street" value="<?php echo $teacher->rue; ?>"></p>
+    <p><span class="label_form"><label>Code postal</label></span><input class="input_form" type="text" name="zipcode" value="<?php echo $teacher->cp; ?>"></p>
+    <p><span class="label_form"><label>Ville</label></span><input class="input_form" type="text" name="city" value="<?php echo $teacher->ville; ?>"></p>
+    <p><span class="label_form"><label>Email</label></span><input class="input_form" type="text" name="email" value="<?php echo $teacher->email; ?>"></p>
+    <?php
 }
 
+if ($v_type == Manager::$STUDENT){
+
+    if (($login != null) && ($password != null) && ($lastName != null)
+        && ($firstName != null) && ($email != null) && ($street != null) 
+            && ($zipcode != null) && ($city != null) && ($photo != null) && ($birthDay != null)){
+        try {
+            $add_people->updateStudent($lastName, $firstName, $birthDay, $street, $zipcode, $city, $email, $login, $password, $photo);
+            echo "eleve ajoute";
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    else {
+        echo "<br>Login : " . ($login != null);
+        echo "<br>password : " . ($password != 0);
+        echo "<br>lastname : " . ($lastName != 0);
+        echo "<br>firstname : " . ($firstName != 0);
+        echo "<br>email : " . ($email != 0);
+        echo "<br>street : " . ($street != 0);
+        echo "<br>zipcode : " . ($zipcode != null);
+        echo "<br>city : " . ($city != 0);
+        echo "<br>photo : " . ($photo != 0);
+        echo "<br>birthday : " . ($birthDay != 0);
+        echo "<br>Veuillez remplir tous les champs correctement";
+
+    }
+
+    $student = $modify_people->getPeopleByID($v_id, $v_type);
+    ?>
+    <p><span class="label_form"><label>Login</label></span><input class="input_form" type="text" name="login" value="<?php echo $student->ulogin; ?>"></p>
+    <p><span class="label_form"><label>Mot de passe</label></span><input class="input_form" type="text" name="password" value="<?php echo $student->passwd; ?>"></p>
+    <p><span class="label_form"><label>Nom</label></span><input class="input_form" type="text" name="lastName" value="<?php echo $student->nom; ?>"></p>
+    <p><span class="label_form"><label>Prenom</label></span><input class="input_form" type="text" name="firstName" value="<?php echo $student->prenom; ?>"></p>
+    <p><span class="label_form"><label>Rue</label></span><input class="input_form" type="text" name="street" value="<?php echo $student->rue; ?>"></p>
+    <p><span class="label_form"><label>Code postal</label></span><input class="input_form" type="text" name="zipcode" value="<?php echo $student->cp; ?>"></p>
+    <p><span class="label_form"><label>Ville</label></span><input class="input_form" type="text" name="city" value="<?php echo $student->ville; ?>"></p>
+    <p><span class="label_form"><label>Email</label></span><input class="input_form" type="text" name="email" value="<?php echo $student->email; ?>"></p>
+    <p><label class="label_form">Photo</label><input class="input_form" type="text" name="photo" value="<?php echo $student->photo; ?>"></p>
+    <p><label class="label_form">Date de naissance</label><input class="input_form" type="text" name="birthDay" value="<?php echo $student->date_naissance; ?>"></p>
+    <?php        
+}
 
 ?>
-
-<table class="sub_body">
-    <tr>
-        <td class="menu">
-            <?php include $menu_adm_file; ?>
-        </td>
-        <td class="content_td">
-            <form method="POST" action="adm-addPeople.htm">
-
-                <select name="status" onchange="window.location.href='adm-addPeople-'+this.selectedIndex+'.htm'">
-                    <option <?php if ($type==0) echo "selected='selected'"; ?> value="0"></option>
-                    <option <?php if ($type==1) echo "selected='selected'"; ?> value="1">Elève</option>
-                    <option <?php if ($type==2) echo "selected='selected'"; ?> value="2">Professeur</option>
-                </select>
-                
-                <?php
-                    if (isset($type)) {
-                        if ($type != 0) {
-                            if ($type == 1) {
-                                echo "<p class='title_form'>Ajout d'un élève</p>";
-                            }
-                            else {
-                                echo "<p class='title_form'>Ajout d'un enseignant</p>";
-                            }
-                        }
-                    }
-                ?>
-
-                <?php
-                    if (isset($type)) {
-                        if ($type != 0) {
-                ?>
-                <p><span class="label_form"><label>Login</label></span><input class="input_form" type="text" name="login"></p>
-                <p><span class="label_form"><label>Mot de passe</label></span><input class="input_form" type="text" name="password"></p>
-                <p><span class="label_form"><label>Nom</label></span><input class="input_form" type="text" name="lastName"></p>
-                <p><span class="label_form"><label>Prenom</label></span><input class="input_form" type="text" name="firstName"></p>
-                <p><span class="label_form"><label>Rue</label></span><input class="input_form" type="text" name="street"></p>
-                <p><span class="label_form"><label>Code postal</label></span><input class="input_form" type="text" name="zipcode"></p>
-                <p><span class="label_form"><label>Ville</label></span><input class="input_form" type="text" name="city"></p>
-                <p><span class="label_form"><label>Email</label></span><input class="input_form" type="text" name="email"></p>
-
-                <?php
-                        }
-                        if ($type==1){
-                ?>
-
-                <!-- si la personne a ajouter est un étudiant -->
-
-                <p><label class="label_form">Photo</label><input class="input_form" type="text" name="photo"></p>
-                <p><label class="label_form">Date de naissance</label><input class="input_form" type="text" name="birthDay"></p>
-                <?php
-                        }
-                        if ($type != 0) {
-                ?>
-                <p><input class="button_form" type="submit" name="envoyer" value="Ajouter personne"></p>
-                <?php
-                        }
-                    }
-                ?>
-
-
-            </form>
-        </td>
-    </tr>
-</table>
